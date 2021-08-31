bfa6635f093e5b2d85c1616d02774b495e8773f2
Kadeem Baldwin <Kadbaldwin@gmail.com>
2021-08-31 15:23:00 -0500

    extract functions

--- Chain.py
+++ Chain.py
@@ -2,8 +2,18 @@ import requests
 import time
 
 
-def children(request):
-    titles = [
+def wiki_link(link):
+    if (
+        link[:6] == "/wiki/"
+        and link[6:].split()[0][:-1] != "Wikipedia:Citation_needed"
+        and link[6:].split()[0][:-1][:4] != "Help"
+    ):
+        return True
+    return False
+
+
+def get_titles(request):
+    return [
         paragraph
         for paragraph in (
             request.text.split("<h2>")[0]
@@ -12,25 +22,25 @@ def children(request):
             .split("</cite>")[-1]
         ).split("<p")
         if paragraph[0] == ">"
-    ][0]
-    return [
-        link[6:].split()[0][:-1]
-        for link in titles.split('<a href="')
-        if link[:6] == "/wiki/"
-        and link[6:].split()[0][:-1] != "Wikipedia:Citation_needed"
-        and link[6:].split()[0][:-1][:4] != "Help"
-    ]
+    ][0].split('<a href="')
 
 
-query = "Emotion"
-print(query)
-cur = children(requests.get(f"https://en.wikipedia.org/wiki/{query}"))[0]
-time.sleep(0.5)
-while cur.lower() != "philosophy":
-    print(cur)
-    cur = children(requests.get(f"https://en.wikipedia.org/wiki/{cur}"))[0]
+def children(request):
+    return [link[6:].split()[0][:-1] for link in filter(wiki_link, get_titles(request))]
+
+
+def chain(query):
+    print(query)
+    cur = children(requests.get(f"https://en.wikipedia.org/wiki/{query}"))[0]
     time.sleep(0.5)
-print(cur)
+    while cur.lower() != "philosophy":
+        print(cur)
+        cur = children(requests.get(f"https://en.wikipedia.org/wiki/{cur}"))[0]
+        time.sleep(0.5)
+    print(cur)
+
+
+chain("Emotion")
 
 """ output:
 
