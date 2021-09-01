4a08bb02e114491b37493b26f61c85e0089b569b
Kadeem Baldwin <Kadbaldwin@gmail.com>
2021-09-01 13:44:39 -0500

    added memo

--- Chain.py
+++ Chain.py
@@ -1,5 +1,59 @@
 import requests
 import time
+from collections import defaultdict
+from functools import reduce
+
+
+class Philosophy_Tree:
+    def __init__(self):
+        self.memo = defaultdict(list)
+
+    def children(self, request):
+        return [
+            link[6:].split()[0][:-1] for link in filter(wiki_link, get_titles(request))
+        ]
+
+    def chain(self, query):
+        if query in self.memo:
+            return self.reduce_print(query)
+        words = [f"{query} -> "]
+        last = query
+        cur = self.children(requests.get(f"https://en.wikipedia.org/wiki/{query}"))[:1]
+        if not cur:
+            return
+        else:
+            cur = cur[0]
+        print(cur)
+        if cur in self.memo:
+            return self.reduce_print(cur)
+        self.memo[last].extend([cur, self.memo[cur]])
+        last = cur
+        time.sleep(0.5)
+        while cur.lower() != "philosophy":
+            words.append(f"{cur} -> ")
+            cur = self.children(requests.get(f"https://en.wikipedia.org/wiki/{cur}"))[
+                :1
+            ]
+            if not cur:
+                return
+            else:
+                cur = cur[0]
+            print(cur)
+            if cur in self.memo:
+                self.memo[last].extend([cur, self.memo[cur]])
+                return self.reduce_print(cur)
+            self.memo[last].extend([cur, self.memo[cur]])
+            last = cur
+            time.sleep(0.5)
+        words.append(cur)
+        return "".join(words)
+
+    def reduce_print(self, query):
+        print(self.memo[query][0])
+        cur = self.memo[query][1]
+        while cur:
+            print(cur[0])
+            cur = cur[1]
 
 
 def wiki_link(link):
@@ -25,26 +79,9 @@ def get_titles(request):
     ][0].split('<a href="')
 
 
-def children(request):
-    return [link[6:].split()[0][:-1] for link in filter(wiki_link, get_titles(request))]
-
-
-def chain(query):
-    words = [f"{query} -> "]
-    cur = children(requests.get(f"https://en.wikipedia.org/wiki/{query}"))[0]
-    time.sleep(0.5)
-    print(cur)
-    while cur.lower() != "philosophy":
-        words.append(f"{cur} -> ")
-        cur = children(requests.get(f"https://en.wikipedia.org/wiki/{cur}"))[0]
-        time.sleep(0.5)
-        print(cur)
-    words.append(cur)
-    return "".join(words)
-
-
 if __name__ == "__main__":
+    pt = Philosophy_Tree()
     while True:
-        chain(input())
+        pt.chain(input())
         print()
 # TODO: add cycle detection for examples like chain("s'more")
